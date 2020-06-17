939168323baa862103edd97bcf0bb1ade204e705
SdBU034 <zhengtao034@163.com>
2020-06-17 21:46:40 +0800

    Update some details

--- django_context_processor/settings.py
+++ django_context_processor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aobj5^icbw!8ss1u6nr$t425cs6*i&n#ik(^3!%w)(o&&!5@&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_context',
         'USER': 'root',
-        'PASSWORD': '******',
+        'PASSWORD': 'lamjj',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist')

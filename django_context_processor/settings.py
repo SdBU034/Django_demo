9cdefde2df4654731c5daf572c9789525a4e5a1e
SdBU034 <zhengtao034@163.com>
2020-07-01 10:45:13 +0800

    Change password

--- django_context_processor/settings.py
+++ django_context_processor/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_context',
         'USER': 'root',
-        'PASSWORD': 'lamjj',
+        'PASSWORD': '******',
         'HOST': 'localhost',
         'PORT': '3306',
     }
